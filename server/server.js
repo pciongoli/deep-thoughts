1a7f1963c7c2c8ac5fa1a9485fbb2a79b3ea2ec4
pcion <patrick.ciongoli@gmail.com>
2022-05-20 22:36:04 -0400

    completed implementation of client-side GraphQL query for homepage data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
+// import the path module
+const path = require("path");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
@@ -20,6 +22,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
