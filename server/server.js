cb835751ac369bf84df79afb146ccc227be24d98
pcion <patrick.ciongoli@gmail.com>
2022-05-22 22:03:40 -0400

    troubleshooting profile page

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
 const express = require("express");
-// import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
-// import the path module
 const path = require("path");
 
-// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
-// import middleware function
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
-// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
    typeDefs,
    resolvers,
@@ -27,7 +22,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
    app.use(express.static(path.join(__dirname, "../client/build")));
 }
-// wildcard Get route * respond to get request with the production-ready REact front-end-code
+
 app.get("*", (req, res) => {
    res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
@@ -35,13 +30,11 @@ app.get("*", (req, res) => {
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
    await server.start();
-   // integrate our Apollo server with the Express application as middleware
    server.applyMiddleware({ app });
 
    db.once("open", () => {
       app.listen(PORT, () => {
          console.log(`API server running on port ${PORT}!`);
-         // log where we can go to test our GQL API
          console.log(
             `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
          );
