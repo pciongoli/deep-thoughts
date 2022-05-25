05b04945c26af0d81ad16b0d22113e0cf50d6279
pcion <patrick.ciongoli@gmail.com>
2022-05-25 00:13:55 -0400

    added ThoughtForm character count functionality and handleFormSubmit button

--- client/src/App.js
+++ client/src/App.js
@@ -51,6 +51,8 @@ function App() {
                      <Route path="/login" element={<Login />} />
                      <Route path="/signup" element={<Signup />} />
                      <Route path="/profile" element={<Profile />} />
+                     <Route path="/profile/:username" element={<Profile />} />
+
                      <Route path="/thought/:id" element={<SingleThought />} />
                      <Route path="*" element={<NoMatch />} />
                   </Routes>
